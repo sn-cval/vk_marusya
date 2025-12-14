1587d1fc752627c725fb861b0aa823880899b84e
Svetlana <svetlana-chistjakova8@rambler.ru>
2025-12-14 15:53:02 +0300

    fix_gitHub_pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vk_marusya/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
